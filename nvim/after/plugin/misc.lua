b30fcfcdcb0da81713f6b72e6b75a7048cb96d1d
sabingurung <sabingurung68@gmail.com>
2024-10-09 20:57:36 +0200

    move to lua config

--- nvim/after/plugin/misc.lua
+++ nvim/after/plugin/misc.lua
@@ -6,7 +6,9 @@ augroup MY_AU_GROUP
     autocmd filetype python nnoremap <buffer> <localleader>ef :sp term://python3 %<CR>
     autocmd filetype lua nnoremap <buffer> <localleader>ef :luafile %<CR>
 augroup END
-let g:conjure#mapping#def_word = v:false
-let g:conjure#client#clojure#nrepl#mapping#refresh_changed = v:false
-let g:conjure#client#clojure#nrepl#connection#auto_repl#enabled = v:false
 ]]
+
+vim.g['conjure#mapping#def_word'] = false
+vim.g['conjure#client#clojure#nrepl#mapping#refresh_changed'] = false
+vim.g['conjure#client#clojure#nrepl#connection#auto_repl#enabled'] = false
+-- vim.g['conjure#relative_file_root'] = vim.fn.getcwd()
